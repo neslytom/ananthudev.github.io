ef20bcc3993de725adbe6528ff0445c6dabd663b
ananthudev <ananthudevdv@gmail.com>
2023-02-23 01:25:54 +0530

    added dummy contact section

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,8 +1,3 @@
-/**
-* PHP Email Form Validation - v3.5
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
 (function () {
   "use strict";
 
@@ -15,10 +10,9 @@
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
+        console.log('The form action property is not set!');
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
@@ -27,55 +21,15 @@
 
       let formData = new FormData( thisForm );
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error)
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
+      php_email_form_submit(thisForm, action, formData);
     });
   });
 
   function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
-  }
-
-  function displayError(thisForm, error) {
+    // Simulate form submission success
     thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
+    thisForm.querySelector('.sent-message').classList.add('d-block');
+    thisForm.reset(); 
   }
 
 })();
--- forms/contact.php
+++ forms/contact.php
@@ -7,35 +7,74 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+//   $receiving_email_address = 'ananthudevdv@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+//   if ( file_exists( $php_email_form = '../assets/vendor/php-email-form/php-email-form.php' ) ) {
+//     include( $php_email_form );
+//   } else {
+//     die( 'Unable to load the "PHP Email Form" Library!' );
+//   }
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+//   // $contact = new PHP_Email_Form;
+//   $contact->ajax = true;
+  
+//   $contact->to = $receiving_email_address;
+//   $contact->from_name = $_POST['name'] ?? '';
+//   $contact->from_email = $_POST['email'] ?? '';
+//   $contact->subject = $_POST['subject'] ?? '';
+//   $contact->message = $_POST['message'] ?? '';
+  
+//   $contact->validate = array(
+//     array('field' => 'name', 'label' => 'Name', 'rules' => 'required'),
+//     array('field' => 'email', 'label' => 'Email', 'rules' => 'required|email'),
+//     array('field' => 'subject', 'label' => 'Subject', 'rules' => 'required'),
+//     array('field' => 'message', 'label' => 'Message', 'rules' => 'required')
+//   );
+  
+//   if ( $contact->validate() ) {
+//     $contact->send();
+//     echo 'success';
+//   } else {
+//     echo $contact->display_errors();
+//   }
+  
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+//   $contact->add_message( $_POST['name'], 'From');
+//   $contact->add_message( $_POST['email'], 'Email');
+//   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
-?>
+//   echo $contact->send();
+$receiving_email_address = 'ananthudevdv@gmail.com';
+
+if ( file_exists( $php_email_form = '../assets/vendor/php-email-form/php-email-form.php' ) ) {
+  include( $php_email_form );
+} else {
+  die( 'Unable to load the "PHP Email Form" Library!' );
+}
+
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
+
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'] ?? '';
+$contact->from_email = $_POST['email'] ?? '';
+$contact->subject = $_POST['subject'] ?? '';
+$contact->message = $_POST['message'] ?? '';
+
+$contact->validate = array(
+  array('field' => 'name', 'label' => 'Name', 'rules' => 'required'),
+  array('field' => 'email', 'label' => 'Email', 'rules' => 'required|email'),
+  array('field' => 'subject', 'label' => 'Subject', 'rules' => 'required'),
+  array('field' => 'message', 'label' => 'Message', 'rules' => 'required')
+);
+
+if ( $contact->validate() ) {
+  $contact->send();
+  echo 'Your message has been sent!';
+} else {
+  echo $contact->display_errors();
+}
+
+ ?>
